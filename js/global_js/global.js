b2a2e873e8d7981d06e432a37ab7f488ae105c49
UmayrNordien <umayrnordien@gmail.com>
2023-07-07 16:26:17 +0200

    update :)

--- js/global_js/global.js
+++ js/global_js/global.js
@@ -1,1 +1,17 @@
 
+//Back to top button
+const backToTopButton = document.querySelector(".back-to-top-btn");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  console.log('%cHome button clicked', 'color: #adadee;');
+  console.log('%cDirected to html top', 'color: #dfb6ee;');
+});
