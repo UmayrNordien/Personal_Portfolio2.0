458064faab78911f8edbb607a1e257cb3951a21e
Umayr Nordien <114750267+UmayrNordien@users.noreply.github.com>
2023-07-13 00:15:28 +0200

    update responsveness in about section <3

--- js/global_js/global.js
+++ js/global_js/global.js
@@ -19,11 +19,41 @@ backToTopButton.addEventListener("click", () => {
 
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
+
+
+
+// let fontSizeAdjusted = false;
+
+// function adjustFontSize() {
+//   if (!fontSizeAdjusted && window.innerWidth < 389) {
+//     // Reduce font size of body, headings, and spans by 5 pixels
+//     document.body.style.fontSize = (parseInt(window.getComputedStyle(document.body).fontSize) - 5) + 'px';
+//     const elements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, span');
+//     for (let i = 0; i < elements.length; i++) {
+//       elements[i].style.fontSize = (parseInt(window.getComputedStyle(elements[i]).fontSize) - 5) + 'px';
+//     }
+//     fontSizeAdjusted = true;
+//   } else if (fontSizeAdjusted && window.innerWidth >= 389) {
+//     // Reset font size of body, headings, and spans to default
+//     document.body.style.fontSize = '';
+//     const elements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, span');
+//     for (let i = 0; i < elements.length; i++) {
+//       elements[i].style.fontSize = '';
+//     }
+//     fontSizeAdjusted = false;
+//   }
+// }
+
+// // Call the function initially on page load
+// adjustFontSize();
+
+// // Call the function on window resize
+// window.addEventListener('resize', adjustFontSize);
