da1787de0d54e77fda309420fe9ec7745b883b13
UmayrNordien <umayrnordien@gmail.com>
2023-07-13 16:27:28 +0200

    update: added back the scroll indicator, working on element responsiveness in project section :)

--- js/global_js/global.js
+++ js/global_js/global.js
@@ -19,14 +19,14 @@ backToTopButton.addEventListener("click", () => {
 
 
 // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
 
-// function myFunction() {
-//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-//   var scrolled = (winScroll / height) * 100;
-//   document.getElementById("myBar").style.width = scrolled + "%";
-// }
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
 
 
 
